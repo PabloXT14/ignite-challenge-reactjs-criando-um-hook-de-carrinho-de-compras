e93022d908a2ab30be6c64e68dacd1e1c6b4bb62
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-12-29 15:16:04 -0300

    Made add addProduct logic

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,57 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    /* ====== Buscando dados do localStorage ===== */
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  /* === Função com lógica de adição de produto no Cart(Carrinho) === */
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+
+      // Checando se produto ja existe no cart
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      // Buscando dados do stock de produtos
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;// pegando qtn em stock do produto
+      const currentAmount = productExists ? productExists.amount : 0;// pegando qtn do produto que o cliente pegou
+      const amount = currentAmount + 1;// nova quantidade de do produto que cliente terá
+
+      // Checando se há quantidade itens suficiente no stock
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');// library de popap do react com mensagem de erro
+        return;
+      }
+
+      // Checando se produto já existe ou não no cart para criar um novo ou adicionar mais uma unidade ao já existente
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        const product = await api.get(`/products/${productId}`);//pegando dados do produto do id informado
+
+        // criando novo produto
+        const newProduct = {
+          ...product.data,
+          amount: 1
+        }
+        updatedCart.push(newProduct);
+      }
+
+      // Atualizando state cart e salvando no localStorage
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
